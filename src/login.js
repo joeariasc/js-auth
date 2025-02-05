7e604b01168fce25676b3cd80b0a9b7aa1a2aaf0
Joe Arias <jarias@wawand.co>
2025-02-05 13:42:28 -0500

    adjusting to read env variables and body request

--- src/login.js
+++ src/login.js
@@ -1,6 +1,8 @@
 export function loginServer(element) {
 	const username = document.querySelector('#username')
 	const password = document.querySelector('#password')
+	const apiServerUrl = import.meta.env.VITE_API_URL
+	console.log('API Server URL:', apiServerUrl)
 	element.addEventListener('click', (event) => {
 		event.preventDefault()
 		if (username.value === '' || password.value === '') {
@@ -9,8 +11,16 @@ export function loginServer(element) {
 		}
 
 		async function fetchData() {
+			// Constructing the clientData object
+			const clientData = {
+				clientType: "web",
+				screenResolution: `${window.screen.width}x${window.screen.height}`,
+				colorDepth: `${window.screen.colorDepth}`,
+				timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+				language: navigator.language
+			};
 			try {
-				const response = await fetch('https://go-auth-qvic.onrender.com/api/auth/login', {
+				const response = await fetch(apiServerUrl + "/api/auth/login", {
 					method: 'POST',
 					headers: {
 						'Content-Type': 'application/json'
@@ -18,21 +28,19 @@ export function loginServer(element) {
 					body: JSON.stringify({
 						username: username.value,
 						password: password.value,
-						clientData:{
-							screenResolution: window.screen.width + "x" + window.screen.height,
-							colorDepth: window.screen.colorDepth,
-							timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-							language: navigator.language
-						}
+						clientData: clientData
 					})
 				})
 
 				if (!response.ok) {
-					throw new Error('Network response was not ok')
+					console.log("error", {
+						status: response.status,
+						statusText: response.statusText
+					})
 				}
 
-				const data = await response.json()
-				console.log(data)
+				const textResponse = await response.text();
+				console.log('Raw Response:', textResponse);
 				// Handle the response data here
 			} catch (error) {
 				console.error('There has been a problem with your fetch operation:', error)
