4ca617f7b74e94eac45a7f75ecffacc420a662b4
Joe Arias <jarias@wawand.co>
2025-02-10 10:01:27 -0500

    adding config to get fingerprint

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const apiServerUrl = import.meta.env.VITE_API_URL;
+
+export const fingerprint = `${window.screen.width}x${window.screen.height}-${navigator.language}-${window.screen.colorDepth}`;
